55a92b9983f717249792c6ba826d8316eef10dda
Otavio <otaviobgoulartt@gmail.com>
2024-08-06 22:38:32 -0300

    initials app

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,17 @@
-
-
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello Patos de Minas!!</h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl p-6 text-center">
+        <h1 className="text-zinc-300">Convide seus amigos para fazer uma viagem</h1>
+        <div>
 
-    </>
+        </div>
+        <p className="text-zinc-500 text-sm">Ao planejar uma viagem pela plann.er você automaticamente concorda <br />
+          com nossos
+          <a href="" className="text-zinc-300 underline"> termos de uso </a>e <a href="" className="text-zinc-300 underline">políticias de privacidade </a>
+        </p>
+      </div>
+    </div>
   );
 }
 
