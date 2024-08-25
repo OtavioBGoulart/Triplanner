bbc51eba5f7b9e3765349fad4265eb957a11dfcd
Otavio <otaviobgoulartt@gmail.com>
2024-08-25 17:59:31 -0300

    feat: add logo

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,11 @@ function App() {
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="max-w-3xl p-6 text-center">
-        <h1 className="text-zinc-300">Convide seus amigos para fazer uma viagem</h1>
-        <div>
-
+        <div className="flex flex-col items-center gap-3">
+          <img src="logo.svg" alt="plann.er" />
+          <h1 className="text-zinc-300">Convide seus amigos e planejem sua próxima viagem</h1>
         </div>
-        <p className="text-zinc-500 text-sm">Ao planejar uma viagem pela plann.er você automaticamente concorda <br />
+        <p className="text-zinc-500 text-sm">Ao planejar a viagem pela plann.er você automaticamente concorda<br />
           com nossos
           <a href="" className="text-zinc-300 underline"> termos de uso </a>e <a href="" className="text-zinc-300 underline">políticias de privacidade </a>
         </p>
