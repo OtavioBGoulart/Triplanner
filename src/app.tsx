d344ab3c6158195a73dedc57d126648130ba2615
Otavio <otaviobgoulartt@gmail.com>
2024-08-25 18:26:50 -0300

    feat: react-router-dom

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CreateTripPage } from "./pages/create-trip";
+import { TripDetailsPage } from "./pages/trip-details";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateTripPage />
+
+  },
+  {
+    path: "/trips",
+    element: <TripDetailsPage />
+  },
+])
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="max-w-3xl p-6 text-center">
-        <div className="flex flex-col items-center gap-3">
-          <img src="logo.svg" alt="plann.er" />
-          <h1 className="text-zinc-300">Convide seus amigos e planejem sua próxima viagem</h1>
-        </div>
-        <p className="text-zinc-500 text-sm">Ao planejar a viagem pela plann.er você automaticamente concorda<br />
-          com nossos
-          <a href="" className="text-zinc-300 underline"> termos de uso </a>e <a href="" className="text-zinc-300 underline">políticias de privacidade </a>
-        </p>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/trip-details/index.tsx
@@ -0,0 +1,6 @@
+export function TripDetailsPage() {
+
+    return (
+        <h1 className="text-zinc-200">Ola, pagina em teste</h1>
+    )
+}
