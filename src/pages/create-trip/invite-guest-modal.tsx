06b8755826988a4935c066f29f0266161761836a
Otavio <otaviobgoulartt@gmail.com>
2024-10-01 23:25:51 -0300

    feat: react-daypicker style

--- src/pages/create-trip/invite-guest-modal.tsx
+++ src/pages/create-trip/invite-guest-modal.tsx
@@ -8,7 +8,10 @@ export function InviteGuestsModal() {
     return (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
             <div className="w-[640px] h-32 rounded-lx py-5 px-6 space-y-5 bg-zinc-900">
-
+                <div className="flex ">
+                    <p className="text-zinc-50 text-lg">Selecionar convidados</p>
+                    <X className="size-5" />
+                </div>
             </div>
         </div>
     )
--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,5 +1,8 @@
 import { MapPin, Calendar, Settings2, ArrowRight, X } from "lucide-react";
 import { Button } from "../../../components/button";
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
 
 interface DestinationandDateStepsProps {
     isGuestsInputOpen: boolean,
@@ -8,6 +11,20 @@ interface DestinationandDateStepsProps {
 }
 
 export function DestinationandDateSteps({ isGuestsInputOpen, openGuestsInput, closeGuestsInput }: DestinationandDateStepsProps) {
+
+    const [isDatePickerIsOpen, setDatePickerIsOpen] = useState(false);
+    const [eventStartAndEndDates, setEventStartAndEndDates] = useState<Date[] | undefined>();
+
+    function openModalDatePicker() {
+        setDatePickerIsOpen(true)
+        console.log("passou aqui")
+    }
+
+    function closeModalDatePicker() {
+        setDatePickerIsOpen(false)
+    }
+
+
     return (
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
 
@@ -20,12 +37,45 @@ export function DestinationandDateSteps({ isGuestsInputOpen, openGuestsInput, cl
                 />
             </div>
 
-            <button className="flex items-center gap-2 text-left w-[1git00px]">
+            <button onClick={openModalDatePicker} disabled={isGuestsInputOpen} className="flex items-center gap-2 text-left w-[1git00px]">
                 <Calendar className="size-5 text-zinc-400" />
                 <span className="text-lg text-zinc-400 w-40 flex-1">
                     Quando?
                 </span>
             </button>
+
+            {isDatePickerIsOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                                <h2 className="font-lg font-semibold text-zinc-50">Selecione a data</h2>
+                                <button>
+                                    <X className="size-5 text-zinc-400" onClick={closeModalDatePicker} />
+                                </button>
+                            </div>
+                        </div>
+
+                        <DayPicker
+                            mode="range"
+                            modifiersClassNames={{
+                                selected: 'bg-lime-300 text-zinc-50', // Estilo para os dias selecionados
+                                today: 'border border-lime-950', // Estilo para o dia atual
+                                range_start: 'bg-green-500 text-white', // Primeiro dia do intervalo
+                                range_end: 'bg-red-500 text-white', // Último dia do intervalo
+                            }}
+                            className="text-zinc-50" // Estilo de cor global do texto
+                            styles={{
+                                caption: { color: 'white' }, // Cor do título do mês
+                                day: { color: 'white' }, // Cor dos números dos dias
+                            }}
+                        />
+
+
+                    </div>
+                </div>
+            )}
+
 
             <div className="w-px h-6 bg-zinc-800" />
 
